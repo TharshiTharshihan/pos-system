ff93cef248b7ffa480fda7146106b6a1e9c15628
TharshiTharshihan <tharshihan2000@gmail.com>
2026-02-27 22:45:35 +0530

    sidebar components setup

--- app/adminDash/page.tsx
+++ app/adminDash/page.tsx
@@ -1,17 +1,23 @@
 "use client";
 
 import React, { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useAppSelector, useAppDispatch } from "../../redux/hooks";
 import { logout } from "../../redux/userSlice";
+import Users from "../../components/Users";
+import Products from "../../components/Products";
+import Image from 'next/image'
+
 
 function Page() {
   const { currentUser, isLoggedIn } = useAppSelector((state) => state.user);
   const dispatch = useAppDispatch();
   const router = useRouter();
+  const searchParams = useSearchParams();
   const [open, setOpen] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [openProfile, setOpenProfile] = useState(false);
+  const tab = searchParams.get("tab");
 
   const handleLogout = async () => {
     try {
@@ -67,12 +73,13 @@ function Page() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 20 20"
+                  width="24"
+                  height="24"
                 >
                   <path
                     stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                   />
                 </svg>
@@ -110,13 +117,13 @@ function Page() {
 
       <p>Current User: {currentUser?.name || "Admin"}</p>
       <p>Status: {isLoggedIn ? "Logged In" : "Not Logged In"}</p>
-      <p>
+      <div>
         {currentUser?.role === "admin" ? (
           <div>Role: Admin</div>
         ) : (
           <div>Role: User</div>
         )}
-      </p>
+      </div>
 
       <nav className="fixed top-0 z-50 w-full bg-neutral-primary-soft border-b border-default">
         <div className="px-3 py-3 lg:px-5 lg:pl-3">
@@ -138,18 +145,19 @@ function Page() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M5 7h14M5 12h14M5 17h10"
                   />
                 </svg>
               </button>
               <a href="https://flowbite.com" className="flex ms-2 md:me-24">
-                <img
+                <Image
                   src="https://flowbite.com/docs/images/logo.svg"
                   className="h-6 me-3"
                   alt="FlowBite Logo"
+                  width={24}
+                  height={24}
                 />
                 <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
                   PoS
@@ -172,10 +180,12 @@ function Page() {
                     onClick={() => setOpenProfile((p) => !p)}
                   >
                     <span className="sr-only">Open user menu</span>
-                    <img
+                    <Image
                       className="w-8 h-8 rounded-full"
                       src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                       alt="user photo"
+                      width={32}           
+                       height={32}
                     />
                   </button>
                 </div>
@@ -255,7 +265,7 @@ function Page() {
       )}
 
       <aside
-        className={`fixed top-0 left-0 z-40 w-64 h-full transition-transform duration-300
+        className={`fixed bg-gradient-to-b from-green-900 to-green-300 top-0 left-0 z-40 w-64 h-full transition-transform duration-300
         ${sidebarOpen ? "translate-x-0" : "-translate-x-full"} sm:translate-x-0`}
         aria-label="Sidebar"
       >
@@ -264,10 +274,12 @@ function Page() {
             href="https://flowbite.com/"
             className="flex items-center ps-2.5 mb-5"
           >
-            <img
+            <Image
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-6 me-3"
               alt="Flowbite Logo"
+              width={24}
+              height={24}
             />
             <span className="self-center text-lg text-heading font-semibold whitespace-nowrap">
               PoS
@@ -276,7 +288,7 @@ function Page() {
           <ul className="space-y-2 font-medium">
             <li>
               <a
-                href="#"
+                href="/adminDash?tab=dashboard"
                 className="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
               >
                 <svg
@@ -289,17 +301,13 @@ function Page() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M10 6.025A7.5 7.5 0 1 0 17.975 14H10V6.025Z"
                   />
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M13.5 3c-.169 0-.334.014-.5.025V11h7.975c.011-.166.025-.331.025-.5A7.5 7.5 0 0 0 13.5 3Z"
                   />
                 </svg>
@@ -308,7 +316,7 @@ function Page() {
             </li>
             <li>
               <a
-                href="#"
+                href="/adminDash?tab=kanban"
                 className="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
               >
                 <svg
@@ -321,10 +329,8 @@ function Page() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M15 5v14M9 5v14M4 5h16a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"
                   />
                 </svg>
@@ -336,7 +342,7 @@ function Page() {
             </li>
             <li>
               <a
-                href="#"
+                href="/adminDash?tab=inbox"
                 className="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
               >
                 <svg
@@ -349,10 +355,8 @@ function Page() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M4 13h3.439a.991.991 0 0 1 .908.6 3.978 3.978 0 0 0 7.306 0 .99.99 0 0 1 .908-.6H20M4 13v6a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-6M4 13l2-9h12l2 9M9 7h6m-7 3h8"
                   />
                 </svg>
@@ -364,7 +368,7 @@ function Page() {
             </li>
             <li>
               <a
-                href="#"
+                href="/adminDash?tab=users"
                 className="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
               >
                 <svg
@@ -377,9 +381,8 @@ function Page() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
                   />
                 </svg>
@@ -388,7 +391,7 @@ function Page() {
             </li>
             <li>
               <a
-                href="#"
+                href="/adminDash?tab=products"
                 className="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
               >
                 <svg
@@ -401,19 +404,19 @@ function Page() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M9 10V6a3 3 0 0 1 3-3v0a3 3 0 0 1 3 3v4m3-2 .917 11.923A1 1 0 0 1 17.92 21H6.08a1 1 0 0 1-.997-1.077L6 8h12Z"
                   />
                 </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Products</span>
+                <span className="flex-1 ms-3 whitespace-nowrap" key="products">
+                  Products
+                </span>
               </a>
             </li>
             <li>
               <a
-                href="#"
+                href="/adminDash?tab=signin"
                 className="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
               >
                 <svg
@@ -426,10 +429,8 @@ function Page() {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round" strokeLinejoin="round"
+                    strokeWidth={2}
                     d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"
                   />
                 </svg>
@@ -439,100 +440,9 @@ function Page() {
           </ul>
         </div>
       </aside>
-
-      <div className="p-2 sm:ml-64 mt-2">
-        <div className="p-2 border-1 border-default border-dashed rounded-base">
-          <div className="grid grid-cols-3 gap-4 mb-4">
-            <div className="flex items-center justify-center h-24 rounded-base bg-blue-300">
-              <p className="text-fg-disabled">
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 12h14m-7 7V5"
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className="flex items-center justify-center h-24 rounded-base bg-blue-300">
-              <p className="text-fg-disabled">
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 12h14m-7 7V5"
-                  />
-                </svg>
-              </p>
-            </div>
-            <div className="flex items-center justify-center h-24 rounded-base bg-blue-300">
-              <p className="text-fg-disabled">
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 12h14m-7 7V5"
-                  />
-                </svg>
-              </p>
-            </div>
-          </div>
-          
-          <div className="flex items-center justify-center h-48 rounded-base bg-blue-300 mb-4">
-            <p className="text-fg-disabled">
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 12h14m-7 7V5"
-                />
-              </svg>
-            </p>
-          </div>
-          <div  className=" border- p-5">
-      
-    </div>
-        </div>
+      <div className="p-2 sm:ml-64">
+        {tab === "users" && <Users />}
+        {tab === "products" && <Products />}
       </div>
     </div>
   );
---
+++ components/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className=' h-screen bg-amber-900 justify-center text-2xl font-bold'>
+      Products
+    </div>
+  )
+}
+
+export default Products
